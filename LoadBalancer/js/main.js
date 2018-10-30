06f1ebef3f0886ca503a26638a54098b74e64953
Molly Setzer <molly.setzer@gmail.com>
2018-10-30 15:48:24 -0700

    edited event handler

--- LoadBalancer/js/main.js
+++ LoadBalancer/js/main.js
@@ -6,7 +6,15 @@
 
 //window.addEventListener('click', eventListenerHandler, false);
 
+var ballArray = [];
+
+// document.getElementById("mod-one-number-button").onclick = function (evt) {
 window.onload = function () {
+    //var countButton = document.getElementById("mod-one-number-button");
+    //countButton.addEventListener('click', createBalls, false);
+
+//    window.addEventListener('scroll', onScrollHandler, false);
+
     document.getElementById("mod-one-number-button").onclick = function (evt) {
         //create balls on screen, qty recieved from getCount()
         function createBalls(ballcount) {
@@ -14,14 +22,19 @@ window.onload = function () {
                 var myimage = new Image(15, 15);
                 myimage.src = 'images/orangeball.png';
                 myimage.id = 'ball' + i;
+                myimage.className = 'resting ballz';
+                myimage.style.marginLeft = ((Math.random() * 2) * i) + "px";
+                myimage.style.marginTop = ((Math.random() * 2) * i) + "px";
+                //also push to ballArray?
+                ballArray.push(myimage.id);
                 document.getElementById("ball-container").appendChild(myimage);
                 onButtonPush();
             }
         }
-        createBalls();
         //get input from "ballCount" field, pass value off to createBalls
         function getCount() {
             var ballCount = window.document.getElementById("ballCount").value;
+      
             createBalls(ballCount);
         }
         getCount();
@@ -29,26 +42,82 @@ window.onload = function () {
         // call getSeconds only on button push 
         function onButtonPush() {
             var buttonClick = window.document.getElementById("mod-one-seconds-button");
-            console.log(buttonClick);
-           // buttonClick.addEventListener("click", getSeconds());
+            buttonClick.addEventListener("click", getSeconds(), false);
             console.log("made it to line 40");
         }
-        onButtonPush();
 
         function getSeconds() {
             var milleseconds = window.document.getElementById("secondsInput").value;
             var seconds = milleseconds * 1000;
             console.log(seconds);
             // createTimer(seconds);
-            transModOne();
+            
+        }
+        //start animations once start button is pressed
+        function startAnimation() {
+            console.log(this);
+            var startButtonClick = window.document.getElementById("start");
+            startButtonClick.addEventListener("click", handleStart());
+            console.log("made it to line 55");
+        }
+        startAnimation();
+
+        function handleStart() {
+            console.log("made it into handleStart");
+        }
+        //***************************currently not doing anything******************************
+        //function transModOne() {
+        //    //get qty of balls, loop through each to give class of tube-animation 
+        //    for (var i; i < ballArray.length; i++) {
+        //        var ballNumber = document.getElementById("ball" + i);
+        //        ballNumber.classList.remove("mod-one-mill");
+        //        ballNumber.classList.add("tube-animation");
+        //    }
+        //}
+
+        function transStart(seconds) {
+            console.log("made it to line 59");
+
+                for (var i = 0; i < ballArray.length; i++) {
+                    var ballNumber = document.getElementById("ball" + i);
+                    if (ballNumber) {
+                        ballNumber.classList.remove("resting");
+                        ballNumber.classList.add("mod-one-mill");
+                        console.log(ballNumber);
+                        modTimer(seconds);
+                        console.log("Made it to line 70");
+                    }
+                
+            }
         }
 
-        function transModOne() {
-            var ballNumber = document.getElementById("ball1");
-            console.log(ballNumber);
-            ballNumber.classList.remove("mod-one-mill");
-            ballNumber.classList.add("tube");
+        function modTimer(seconds) {
+            for (var i = 0; i < ballArray.length; i++) {
+                //setTimeout(move2, 20);
+                console.log("made it to line 76!");
+            }
         }
+
+ //       function move2() {
+ ////         var moveTimer = window.setTimeout()
+ //           for (var i = 0; i < ballArray.length; i++) {
+ //               //if next module is accepting?
+ //               var ballNumber = document.getElementById("ball" + i);
+ //               if (ballNumber) {
+ //                   ballNumber.classList.remove("mod-one-mill");
+ //                   ballNumber.classList.add("tube-animation");
+ //               }
+ //           }
+ //       }
+
+        function thisModule() {
+            this.location = location;
+            this.timer = timer;
+            this.isAccepting = false;
+        }
+        // when they should move to next section change this attribute - thisModule.isAccepting = true;
+
+
         function createTimer(seconds) {
             //get timer length, apply this length to module 1 class/object/whatever
         }
@@ -56,80 +125,6 @@ window.onload = function () {
     }
 }
 
-function sayHello() {
-    var messageDialog = new Windows.UI.Popups.MessageDialog("Hello World!", "Alert");
-    messageDialog.showAsync();
-}
 
 
 
-
-//function timedText() {
-//    setTimeout(myTimeout1, 2000)
-//    setTimeout(myTimeout2, 4000)
-//    setTimeout(myTimeout3, 6000)
-//}
-//function myTimeout1() {
-//    document.getElementById("demo").innerHTML = "2 seconds";
-//}
-//function myTimeout2() {
-//    document.getElementById("demo").innerHTML = "4 seconds";
-//}
-//function myTimeout3() {
-//    document.getElementById("demo").innerHTML = "6 seconds";
-//}
-
-// class Module1 {
-//     constructor(seconds, width) {
-//       this.seconds = seconds;
-//       this.width = width;
-//     }
-//     // Getter
-//     get area() {
-//       return this.postSeconds();
-//     }
-//     // Method
-//     postSeconds() {
-//       return this.seconds * this.width;
-//     }
-//   }
-
-//   const square = new Rectangle(10, 10);
-
-//   console.log(square.area); // 100
-
-var ball1 = null;
-
-
-//function timerOne() {
-    //on click (in html)   
-    //get timer length from input
-    //var seconds = window.document.getElementById("secondsInput").value;
-    // *** TO DO ***
-    // validate input -- see checkInp() below
-    // function checkInp()
-    // {
-    //   var x=document.forms["myForm"]["age"].value;
-    //   if (isNaN(x)) 
-    //   {
-    //     alert("Must input numbers");
-    //     return false;
-    //   }
-    // }
-    //  console.log(seconds);
-    //change input to milleseconds
-    //var seconds = seconds * 1000;
-    //pass seconds value to next function
-    // function setSeconds(seconds)
-    //timer starts
-    // setTimeout(myTimeout1, x);
-
-//}
-//timerOne();
-
-
-// function setSeconds(seconds) {
-//with input from timerOne, set timer to move ball
-// }
-
-//needs to ONLY be called when user clicks submit
